91e5ea6a5c4cbd65827ebdfd11fd11a986c6a51e
Olive Iosello <oiosello@gmail.com>
2022-04-20 20:46:20 -0400

    Created Owner model (issue #1)

---
+++ src/main/scala/petclinic/models/Owner.scala
@@ -0,0 +1,36 @@
+package petclinic.models
+
+import zio._
+import java.util.UUID
+
+
+
+// wraps UUID as a specific OwnerId so that we cannot use an incorrect UUID
+case class OwnerId(id: UUID) extends AnyVal
+
+object OwnerId {
+  def random: ZIO[Random, Nothing, OwnerId] = Random.nextUUID.map(OwnerId(_))
+}
+
+
+// extracts address to reduce the number of fields in owner
+case class Address(ownerId: OwnerId, street: String, city: String, state: String, zip: String)
+
+object Address {
+  def create(ownerId: OwnerId, street: String, city: String, state: String, zip: String): Address =
+    Address(ownerId, street, city, state, zip)
+}
+
+
+// represents an owner of a pet
+case class Owner(id: OwnerId, firstName: String, lastName: String, address: Address, ph: String)
+
+object Owner {
+  def create(firstName: String, lastName: String, address: Address, ph: String): ZIO[Random, Nothing, Owner] = {
+    OwnerId.random.map(id => Owner(id , firstName, lastName, address, ph))
+  }
+}
+
+
+
+
