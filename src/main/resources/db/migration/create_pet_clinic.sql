c2025ac437be3d3a261c814955771db0e598d5b7
Olive Iosello <oiosello@gmail.com>
2022-04-28 15:33:51 -0400

    Create Appt Sql migration

--- src/main/resources/db/migration/create_pet_clinic.sql
+++ src/main/resources/db/migration/create_pet_clinic.sql
@@ -16,3 +16,25 @@ CREATE TABLE IF NOT EXISTS pet (
     owner_id uuid NOT NULL REFERENCES owner(id) ON DELETE CASCADE
 );
 
+CREATE TABLE IF NOT EXISTS vet {
+     id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+     last_name text NOT NULL,
+     specialty test NOT NULL
+ };
+
+ CREATE TABLE IF NOT EXISTS appt {
+     id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+     date date NOT NULL,
+     description text NOT NULL,
+     vet_id uuid NOT NULL REFERENCES vet(id) ON DELETE CASCADE,
+     pet_id uuid NOT NULL REFERENCES pet(id) ON DELETE CASCADE
+ };
+
+
+ -- Vet data for display list
+ INSERT INTO vet (last_name, specialty) VALUES ('Dr. Baloo', 'Cardiology');
+ INSERT INTO vet (last_name, specialty) VALUES ('Dr. Hedwig', 'Oncology');
+ INSERT INTO vet (last_name, specialty) VALUES ('Dr. Totoro', 'Large Animal Internal Medicine');
+ INSERT INTO vet (last_name, specialty) VALUES ('Dr. Rajah', 'Dentistry');
+ INSERT INTO vet (last_name, specialty) VALUES ('Dr. Sid', 'Neurology');
+ INSERT INTO vet (last_name, specialty) VALUES ('Dr. Stuart', 'Small Animal Internal Medicine
