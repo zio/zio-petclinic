babe7de0cf5da4e2f44d7d63a37ce2b3956f7e30
Olive Iosello <oiosello@gmail.com>
2022-04-28 15:33:51 -0400

    Create Appt Sql migration

--- src/main/resources/db/migration/create_pet_clinic.sql
+++ src/main/resources/db/migration/create_pet_clinic.sql
@@ -17,17 +17,24 @@ CREATE TABLE IF NOT EXISTS pet (
 );
 
 CREATE TABLE IF NOT EXISTS vet {
-    id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
-    first_name text NOT NULL,
-    last_name text NOT NULL,
-    specialty test NOT NULL
-};
+     id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+     last_name text NOT NULL,
+     specialty test NOT NULL
+ };
 
--- Vet data for display list
-INSERT INTO vet (first_name, last_name, specialty) VALUES ('Dr.', 'Baloo', 'Cardiology');
-INSERT INTO vet (first_name, last_name, specialty) VALUES ('Dr.', 'Hedwig', 'Oncology');
-INSERT INTO vet (first_name, last_name, specialty) VALUES ('Dr.', 'Totoro', 'Large Animal Internal Medicine');
-INSERT INTO vet (first_name, last_name, specialty) VALUES ('Dr.', 'Rajah', 'Dentistry');
-INSERT INTO vet (first_name, last_name, specialty) VALUES ('Dr.', 'Sid', 'Neurology');
-INSERT INTO vet (first_name, last_name, specialty) VALUES ('Dr.', 'Stuart', 'Small Animal Internal Medicine');
+ CREATE TABLE IF NOT EXISTS appt {
+     id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+     date date NOT NULL,
+     description text NOT NULL,
+     vet_id uuid NOT NULL REFERENCES vet(id) ON DELETE CASCADE,
+     pet_id uuid NOT NULL REFERENCES pet(id) ON DELETE CASCADE
+ };
 
+
+ -- Vet data for display list
+ INSERT INTO vet (last_name, specialty) VALUES ('Dr. Baloo', 'Cardiology');
+ INSERT INTO vet (last_name, specialty) VALUES ('Dr. Hedwig', 'Oncology');
+ INSERT INTO vet (last_name, specialty) VALUES ('Dr. Totoro', 'Large Animal Internal Medicine');
+ INSERT INTO vet (last_name, specialty) VALUES ('Dr. Rajah', 'Dentistry');
+ INSERT INTO vet (last_name, specialty) VALUES ('Dr. Sid', 'Neurology');
+ INSERT INTO vet (last_name, specialty) VALUES ('Dr. Stuart', 'Small Animal Internal Medicine
