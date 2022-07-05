f3a752b3e89e6773effc985b333493c64e086c44
Olive Iosello <oiosello@gmail.com>
2022-07-05 19:51:18 -0400

    add documentation to owner service

--- backend/src/main/scala/petclinic/services/OwnerService.scala
+++ backend/src/main/scala/petclinic/services/OwnerService.scala
@@ -1,20 +1,35 @@
 package petclinic.services
 
+import petclinic.models._
 import zio._
 import zio.macros._
-import petclinic.models._
 
+/** OwnerService manages the CRUD operations for the Owner type. Services like
+  * this are responsible for persisting and modifying saved data. Note that the
+  * "@accessible" macro annotation is used to add "accessors" to the companion
+  * object for use in the corresponding spec.
+  */
 @accessible
 trait OwnerService {
 
+  /** Creates a new Owner
+    */
   def create(firstName: String, lastName: String, address: String, phone: String, email: String): Task[Owner]
 
+  /** Deletes an existing Owner
+    */
   def delete(id: OwnerId): Task[Unit]
 
+  /** Retreives an Owner from the database
+    */
   def get(id: OwnerId): Task[Option[Owner]]
 
+  /** Retreives all Owners from the database
+    */
   def getAll: Task[List[Owner]]
 
+  /** Updates an existing Owner
+    */
   def update(
       id: OwnerId,
       firstName: Option[String] = None,
--- backend/src/main/scala/petclinic/services/OwnerServiceLive.scala
+++ backend/src/main/scala/petclinic/services/OwnerServiceLive.scala
@@ -1,35 +1,61 @@
 package petclinic.services
 
-import zio._
 import petclinic.QuillContext
 import petclinic.models.{Owner, OwnerId}
+import zio._
 
 import javax.sql.DataSource
 
+/** OwnerServiceLive is a service which provides the "live" implementation of
+  * the OwnerService. This implementation uses a DataSource, which will
+  * concretely be a connection pool.
+  */
 final case class OwnerServiceLive(dataSource: DataSource) extends OwnerService {
 
+  // QuillContext needs to be imported here to expose the methods in the QuillContext object.
   import QuillContext._
 
+  /** create uses the make method defined in the Owner model to create a new
+    * Owner. The Owner is formatted into a query string, then inserted into the
+    * database using provideEnvironment to provide the datasource to the effect
+    * returned by run. ZEnvironment is used to provide the necessary dataSource
+    * to the insertValue method. The created Owner is returned.
+    */
   override def create(firstName: String, lastName: String, address: String, phone: String, email: String): Task[Owner] =
     for {
       owner <- Owner.make(firstName, lastName, address, phone, email)
       _     <- run(query[Owner].insertValue(lift(owner))).provideEnvironment(ZEnvironment(dataSource))
     } yield owner
 
+  /** delete uses the filter method to find an Owner in the database whose ID
+    * matches the one provided and deletes it. Unit is returned to indicate that
+    * we are running this method for its side effects, a deleted user gives us
+    * no information. This will either fail or succeed.
+    */
   override def delete(id: OwnerId): Task[Unit] =
     run(query[Owner].filter(_.id == lift(id)).delete)
       .provideEnvironment(ZEnvironment(dataSource))
       .unit
 
+  // get uses the filter method to find an Owner in the database whose ID matches the one provided and returns it.
   override def get(id: OwnerId): Task[Option[Owner]] =
     run(query[Owner].filter(_.id == lift(id)))
       .provideEnvironment(ZEnvironment(dataSource))
       .map(_.headOption)
 
+  // getAll uses the query method to find all entries in the database of type Owner and returns them.
   override def getAll: Task[List[Owner]] =
     run(query[Owner])
       .provideEnvironment(ZEnvironment(dataSource))
 
+  /** update uses the filter method to find an Owner in the database whose ID
+    * matches the one provided and updates it with the provided optional values.
+    * Because a user may not provide all optional values, setOpt is used to
+    * preserve the existing value in the case one is not provided to replace it.
+    * Unit is returned to indicate side-effecting code. Note that this
+    * dynamicQuery is not generated at compile time.
+    * https://getquill.io/#writing-queries-dynamic-queries
+    */
   override def update(
       id: OwnerId,
       firstName: Option[String],
@@ -54,6 +80,9 @@ final case class OwnerServiceLive(dataSource: DataSource) extends OwnerService {
 
 }
 
+/** Here in the companion object we define the layer that provides the live
+  * implementation of the OwnerService.
+  */
 object OwnerServiceLive {
 
   val layer: URLayer[DataSource, OwnerService] = ZLayer.fromFunction(OwnerServiceLive.apply _)
